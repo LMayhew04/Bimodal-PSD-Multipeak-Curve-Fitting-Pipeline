4a6fc51ad67a3b9c71792548690239c2aec586db
Logan Mayhew <loganmayhew@usconec.com>
2025-07-02 12:26:28 -0400

    Cleaning up

--- MatLab Bimodal Curve Fitting/Temporary/pdfHelpers/normalPDF.m
+++
@@ -1,4 +0,0 @@
-function y = normalPDF(x, mu, sigma, A)
-%NORMALPDF  Normal distribution PDF with amplitude scaling.
-y = A * exp(-0.5 * ((x-mu)/sigma).^2) ./ (sigma * sqrt(2*pi));
-end
--- MatLab Bimodal Curve Fitting/Temporary/pdfHelpers/raylPDF.m
+++
@@ -1,4 +0,0 @@
-function y = raylPDF(x, sigma, A)
-%RAYLPDF  Rayleigh PDF with amplitude scaling.
-y = A * (x/sigma^2) .* exp(-x.^2/(2*sigma^2));
-end
